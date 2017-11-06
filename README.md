75f2458abc89b0740747d46d0688135fdaeed36c
Daniel Hsing <hsing.daniel@gmail.com>
2017-11-07 07:49:35 +0800

    feat: initial commit
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# gci-csv-validator
+
+CSV validator for the [Google Code-in Web API](https://developers.google.com/open-source/gci/api)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="gci-validator",
+    version="0.0.1",
+    author="Daniel Hsing",
+    author_email="hsing.daniel@gmail.com",
+    license="LICENSE"
+)
