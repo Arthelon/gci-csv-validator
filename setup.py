fddfb80f01df866199e57c79df5e3a814b753661
Daniel Hsing <hsing.daniel@gmail.com>
2017-11-07 12:45:22 +0800

    feat: publish on pypi add basic validation
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
 
 setup(
     name="gci-validator",
-    version="0.0.1",
+    version="0.0.2",
+    long_description=readme(),
+    description="CSV validator for the Google Code-in web API",
     author="Daniel Hsing",
     author_email="hsing.daniel@gmail.com",
-    license="LICENSE"
+    license="LICENSE",
+    scripts=["bin/gci-validator"],
+    install_requires=[
+        "docopt"
+    ]
 )
